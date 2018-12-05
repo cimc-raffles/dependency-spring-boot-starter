500ea8618a7e8cc39a32a4d4cd5a469a2227451f
cpp <x@y.z>
2018-12-05 12:52:36 +0800

    first commit

---
+++ src/main/java/cimc/raffles/dependency/StarterDependencyApplication.java
@@ -0,0 +1,10 @@
+package cimc.raffles.dependency;
+
+//@org.springframework.boot.autoconfigure.SpringBootApplication
+public class StarterDependencyApplication 
+{
+//	public static void main(String[] args)
+//	{	
+//		org.springframework.boot.SpringApplication.run(StarterDependencyApplication.class, args);
+//	}
+}
---
+++ src/test/java/cimc/raffles/dependency/StarterDependencyApplicationTests.java
@@ -0,0 +1,16 @@
+package cimc.raffles.dependency;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StarterDependencyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
